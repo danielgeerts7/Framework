0b2d3f1fc195cafa1e26a9289adc88177fcfc240
danielgeerts7 <daniel.geerts@live.nl>
2015-12-10 14:28:05 +0100

    Changed 32x32 pix to 64x64 pix

--- rt2d/config.h
+++ rt2d/config.h
@@ -21,8 +21,8 @@
 
 
 // screenwidth, screenheight
-#define SWIDTH 1280 ///< The screen width
-#define SHEIGHT 720 ///< The screen height
+#define SWIDTH 1600 ///< The screen width
+#define SHEIGHT 900 ///< The screen height
 
 #define FULLSCREEN 0 ///< Fullscreen or not
 
