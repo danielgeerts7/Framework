e05d69f98a7fcc564d0015764141273999578817
danielgeerts7 <daniel.geerts@live.nl>
2015-12-11 11:48:48 +0100

    Deleted some copyright, added my own stuff

--- start/loadtiles.cpp
+++ start/loadtiles.cpp
@@ -9,37 +9,16 @@ using namespace std;
 
 LoadTiles::LoadTiles() : BasicEntity()
 {
-	/*
-	IN 'assets/tiles_2.cvs' :::::::
-	Needs to work:
-	0,1,2,3,4,5
-	6,7,8,9,10,11
-	12,13,14,15,16,17
-	18,19,20,21,22,23
-	24,25,26,27,28,29
-	30,31,32,33,34,35
-
-	Works now:
-	30,24,18,12,6,0
-	31,25,19,13,7,1
-	32,26,20,14,8,2
-	33,27,21,15,9,3
-	34,28,22,16,10,4
-	35,29,23,17,11,5
-
-	OR THERE IS SOMETHING WRONG WITH THE 'ADDGRID' OR ELSE WITH THE IFSTREAM FILE CHECKER
-	*/
-	
-	gridwidth = 25;
-	gridheight = 25;
+	gridwidth = 3;
+	gridheight = 3;
 	cellwidth = 64;
 	cellheight = 64;
 	tileCounter = 0;
 	this->position = Point2(0, 0);
-	this->addGrid("assets/tiles_1.tga", 6, 6, gridwidth, gridheight, cellwidth, cellheight);
-	
+	this->addGrid("assets/tiles_numbers.tga", 3, 3, gridwidth, gridheight, cellwidth, cellheight);
+
 	string line, cell;
-	ifstream myfile("assets/tiles_1.csv");
+	ifstream myfile("assets/tiles_numbers.csv");
 	// try to open the file, else if you cannot throw exception
 	try
 	{
