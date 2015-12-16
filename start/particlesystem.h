5afd722ef2f2ba293499e18ce65d4e9802e112ef
danielgeerts7 <daniel.geerts@live.nl>
2015-12-16 10:20:38 +0100

    Added particlesystem class

---
+++ start/particlesystem.h
@@ -0,0 +1,18 @@
+/**
+* This file is part of a demo that shows how to use RT2D, a 2D OpenGL framework.
+*
+* - Copyright 2015 Daniel Geerts <daniel.geerts@live.nl>
+*     - Initial commit
+*/
+
+#ifndef PARTICLESYSTEM_H
+#define PARTICLESYSTEM_H
+
+class ParticleSystem
+{
+public:
+	ParticleSystem();
+	~ParticleSystem();
+};
+
+#endif /* PARTICLESYSTEM_H */
