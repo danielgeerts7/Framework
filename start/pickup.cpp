1c9228016f77bd953635114df87210411152cb86
danielgeerts7 <daniel.geerts@live.nl>
2016-01-15 10:27:22 +0100

    Initial pickup class and .tga

---
+++ start/pickup.cpp
@@ -0,0 +1,15 @@
+/**
+* - Copyright 2015 Daniel Geerts <daniel.geerts@live.nl>
+*     - Initial commit
+*/
+
+#include "pickup.h"
+
+Pickup::Pickup()
+{
+}
+
+
+Pickup::~Pickup()
+{
+}
---
+++ start/pickup.h
@@ -0,0 +1,18 @@
+/**
+* - Copyright 2015 Daniel Geerts <daniel.geerts@live.nl>
+*     - Initial commit
+*/
+
+#ifndef PICKUP_H
+#define PICKUP_H
+
+class Pickup
+{
+	public:
+		Pickup();
+		~Pickup();
+
+	private:
+};
+
+#endif /* PICKUP_H */
